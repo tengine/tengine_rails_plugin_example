17478e652c6db9dda1fd4e5997b06e3d079b079d
akima <akm2000@gmail.com>
2012-11-05 15:54:00 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TengineRailsPluginExample::Application.config.secret_token = '27a2fd508fdf0719937ed6a71c600c486285d7d42a7f2c80774d1bd73819bffe1f41c55147ed1ec0f908fa126c0d75426bb8ca4672a07adf9d8d666bd3f024fb'
